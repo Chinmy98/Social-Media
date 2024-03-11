34a0fdb5bcc0f270ffe2e6ce88de65c3cb120f3b
Indumathi Chinmy Greeshma <chinmy.careers@gmail.com>
2024-03-11 16:11:41 +0530

    Fetched data using UseEffect hook insted of fetch

--- indu-sm/src/components/WelcomeMessage.jsx
+++ indu-sm/src/components/WelcomeMessage.jsx
@@ -2,12 +2,6 @@ const WelcomeMessage = ({getOnClickFeatch}) => {
   return <div className="bg-light text-secondary px-4 py-5 text-center">
     <div className="py-5">
       <h1 className="display-5 fw-bold text-black">No Post Featched</h1>
-      <div className="col-lg-6 mx-auto">
-        <p className="fs-5 mb-4">Please click below button to featch your posts</p>
-        <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          <button type="button" onClick={getOnClickFeatch} className="btn btn-outline-primary btn-lg px-4 me-sm-3 fw-bold">Featch Post</button>
-        </div>
-      </div>
     </div>
   </div>
 
