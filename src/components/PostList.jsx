dd36b37025a59157f04d54ec62c57d0bfb13182a
Indumathi Chinmy Greeshma <chinmy.careers@gmail.com>
2024-03-13 16:35:29 +0530

    Fixed issue with spinner

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,39 +1,36 @@
 import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
-import LoadingSpinner from "./LoadingSpinner";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
-  const [Fetching, setFetching] = useState(false);
-  const Controller = AbortController();
-  const signal = Controller;
+  const [fetching, setFetching] = useState(false);
+
   useEffect(() => {
     setFetching(true);
-    fetch('https://dummyjson.com/posts', { signal })
-      .then(res => res.json())
-      .then(data => {
-        addInitialPosts(data.posts)
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch("https://dummyjson.com/posts", { signal })
+      .then((res) => res.json())
+      .then((data) => {
+        addInitialPosts(data.posts);
         setFetching(false);
       });
-  
+
     return () => {
-      Controller.abort();
-    }
-    
-  },[]);
+      console.log("Cleaning up UseEffect.");
+      controller.abort();
+    };
+  }, []);
+
   return (
     <>
-      {
-        Fetching && <LoadingSpinner/>
-      }
-      {
-        !Fetching && postList.length === 0 && <WelcomeMessage />
-      }
-      {!Fetching && postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && <WelcomeMessage />}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
     </>
   );
 };
