859d37ba5874375b8a5bc0591936e67c1bbf31e9
Indumathi Chinmy Greeshma <chinmy.careers@gmail.com>
2024-03-13 15:47:02 +0530

    Added loading spinner and cleaned the UseEffect

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,16 @@
+const LoadingSpinner = () => {
+  return <div>
+    <center>
+      <img
+            src="https://media.tenor.com/I6kN-6X7nhAAAAAi/loading-buffering.gif"
+            alt=""
+            width="32"
+            height="32"
+            className="spinner"
+      />
+    </center>
+  </div>
+
+}
+
+export default LoadingSpinner
